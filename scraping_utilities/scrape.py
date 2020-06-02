eb637df464feda06cc8c0a099a0eec1b4d17085b
Bhaskar <bhaskar2choudhary@gmail.com>
2020-06-02 16:36:16 +0530

    bug fixing

--- scraping_utilities/scrape.py
+++ scraping_utilities/scrape.py
@@ -33,9 +33,68 @@ from awards_scrape import *
 
 
 if __name__ == "__main__":
-    from utilities import get_proxies
+    from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+    import requests
+    import pandas as pd
+    import numpy as np
+    from multiprocessing import Pool
 
-    config = yaml.safe_load(open('./../config.yml'))
+    def parallelize_validation(proxies, func, n_cores):
+        print(1)
+        df_proxies = pd.DataFrame(proxies).rename(columns={0: 'proxy'})
+        print(2)
+        df_split = np.array_split(df_proxies, n_cores)
+        print(3)
+
+        pool = Pool(n_cores)
+        print(4)
+        df = pd.concat(pool.map(func, df_split))
+        print(5)
+        pool.close()
+        pool.join()
+        return df
+
+
+    def validate_proxies(df_proxies):
+        print(6)
+        proxies = list(df_proxies['proxy'].unique())
+        title_id = 'tt0111161'  # The Shawshank Redemption
+
+        valid_proxies = []
+        for proxy in proxies:
+            print('Validating proxy -', proxy)
+
+            proxyDict = {
+                "http": 'http://' + proxy,
+                "https": 'https://' + proxy,
+                "ftp": 'ftp://' + proxy
+            }
+            try:
+                html_content = requests.get("http://www.imdb.com/title/" + title_id, proxies=proxyDict).text
+                if html_content.count('title_wrapper') != 0:
+                    valid_proxies.append(proxy)
+            except:
+                pass
+
+        df = pd.DataFrame(valid_proxies).rename(columns={0: 'valid_proxy'})
+
+        return df
+
+
+    def get_proxies(n_cores):
+        proxies = []
+        req_proxy = RequestProxy()
+        for proxy in req_proxy.get_proxy_list():
+            proxies.append(proxy.ip + ':' + str(proxy.port))
+
+        proxies = list(set(proxies))
+        print(len(proxies), 'proxies gathered.')
+        print('Starting to validate proxies...')
+        df_proxies = parallelize_validation(proxies, validate_proxies, n_cores)
+        proxies = list(df_proxies['valid_proxy'].unique())
+        print('Remaining proxies after validation -', len(proxies))
+
+        return proxies
 
     print('Requesting proxies...')
     proxies = get_proxies(config['algo']['vCPU'])
