509ad0afe47996d79bf972f198389c612199fc53
Bhaskar <bhaskar2choudhary@gmail.com>
2020-06-08 21:57:10 +0530

    updated movie_synopsis script for beautiful soup

--- scraping_utilities/movies/movie_synopsys_scrape.py
+++ scraping_utilities/movies/movie_synopsys_scrape.py
@@ -1,145 +1,86 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-from selenium import webdriver
+
 import pandas as pd
 from datetime import datetime
-from selenium.common.exceptions import TimeoutException
-import time
-from sqlalchemy import create_engine
 import yaml
+from bs4 import BeautifulSoup
+
+import sys
+sys.path.extend(['./..'])
+
+from utilities import get_session, should_go_ahead
 
 
-def movie_synopsys_scrape(titles):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--window-size=800x800')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('log-level=3')
-    # options.add_argument('--proxy-server=45.76.226.206:3128')
+def movie_synopsys_scrape(df_titles):
+    titles = list(df_titles['titles'])
 
     config = yaml.safe_load(open('./../config.yml'))
     data_folder = config['movies_data_folder']
-
-    driver = webdriver.Chrome(config['chromedriver'], chrome_options=options)
 
     scrape_start_time = datetime.now()
     i = 1
     j = 0
 
-    try:
-        df_movie_synopsis_already_scraped = pd.read_csv(data_folder+'movie_synopsys.csv')
-        titles_scraped = list(df_movie_synopsis_already_scraped['title_id'])
-        del df_movie_synopsis_already_scraped
-    except:
-        titles_scraped = []
-    print('Scraping history read...')
-
+    session = get_session()
     plots = []
 
     for title_id in titles:
-        if titles_scraped.count(title_id) == 0:
+        url = 'https://www.imdb.com/title/'+title_id+'/plotsummary'
+        go_ahead, session, html_content = should_go_ahead(url, session, 'subpage_title_block')
+
+        if go_ahead:
             try:
-                driver.get('https://www.imdb.com/title/'+title_id+'/plotsummary')
-                try:
-                    summaries = driver.find_element_by_id('plot-summaries-content').text.split('\n')
-                    go_ahead = True
-                except:
-                    errors = driver.find_elements_by_class_name('error_message')
-                    if errors:
-                        if errors[0].text.count('URL was not found') == 0:
-                            print('Sleeping for 2 minutes...')
-                            time.sleep(2*60)
-                            try:
-                                driver.get('https://www.imdb.com/title/' + title_id + '/plotsummary')
-                                summaries = driver.find_element_by_id('plot-summaries-content').text.split('\n')
-                                go_ahead = True
-                            except:
-                                go_ahead = False
-                        else:
-                            go_ahead = False
-                    else:
-                        if driver.find_element_by_xpath("//*").text == '':
-                            print('Sleeping for 2 minutes...')
-                            time.sleep(2*60)
-                            try:
-                                driver.get('https://www.imdb.com/title/'+title_id+'/plotsummary')
-                                summaries = driver.find_element_by_id('plot-summaries-content').text.split('\n')
-                                go_ahead = True
-                            except:
-                                go_ahead = False
-                        else:
-                            go_ahead = False
+                soup = BeautifulSoup(html_content, 'html.parser')
 
-                if go_ahead:
-                    synopsys = driver.find_element_by_id('plot-synopsis-content').text
+                summaries = soup.find('ul', id='plot-summaries-content').findAll('li', class_='ipl-zebra-list__item')
+                summaries = [item.find('p').text.strip() for item in summaries]
 
-                    plots.append({
-                        'title_id':title_id,
-                        'summaries':summaries,
-                        'synopsys':synopsys
-                        })
-                else:
-                    print('Skipping', title_id, 'as code broke for it.')
-                    j += 1
-            except TimeoutException as ex:
-                titles.append(title_id)
+                synopsys = soup.find('ul', id='plot-synopsis-content').find('li', class_='ipl-zebra-list__item').text.strip()
+
+                plots.append({
+                    'title_id':title_id,
+                    'summaries':summaries,
+                    'synopsys':synopsys
+                    })
+            except Exception as e:
+                print('Skipping', title_id, '-', e)
                 print('\n')
-                print('Skipping '+title_id+' because of Selenium TimeoutException')
-                print('\n')
-                del driver
-                driver = webdriver.Chrome(config['chromedriver'], chrome_options=options)
-            if i%25 == 0:
-                print('Movies scraped -',i)
-
-                time_since_start = (datetime.now()-scrape_start_time).seconds
-                all_time_scraping_speed = (i/time_since_start)*3600
-                if time_since_start < 60:
-                    time_since_start = str(time_since_start)+' seconds'
-                elif time_since_start < 3600:
-                    time_since_start = str(time_since_start//60)+ ':'+str(time_since_start%60)+' minutes'
-                else:
-                    time_since_start = str(time_since_start//3600)+ ':'+str((time_since_start%3600)//60)+' hours'
-                print('Time since scraping started - '+time_since_start)
-                print('All time scraping speed - '+('%.0f'%(all_time_scraping_speed))+' movies/hour')
-
-                try:
-                    time_since_last_checkpoint = (datetime.now()-time_checkpoint).seconds
-                except:
-                    time_since_last_checkpoint = (datetime.now()-scrape_start_time).seconds
-                current_scraping_speed = (25/time_since_last_checkpoint)*3600
-                time_remaining = (time_since_last_checkpoint*((len(titles)-i-j)/25))/(3600)
-                print('Current scraping speed - '+('%.0f'%(current_scraping_speed))+' movies/hour')
-                print('Time remaining as per current speed - '+('%.1f'%(time_remaining))+' hours')
-                print('\n')
-                time_checkpoint = datetime.now()
-
-            if i%100 == 0:
-                if len(plots) > 0:
-                    try:
-                        df_movie_synopsys = pd.read_csv(data_folder+'movie_synopsys.csv')
-                    except:
-                        df_movie_synopsys = pd.DataFrame()
-
-                    df_movie_synopsys = df_movie_synopsys.append(plots)
-                    df_movie_synopsys.to_csv(data_folder+'movie_synopsys.csv', index=False)
-                    del df_movie_synopsys
-                    plots = []
-            i += 1
+                j += 1
         else:
+            print('Skipping', title_id, '- something wrong.')
+            print('\n')
             j += 1
+        if i%25 == 0:
+            print('Movies scraped -',i)
 
+            time_since_start = (datetime.now()-scrape_start_time).seconds
+            all_time_scraping_speed = (i/time_since_start)*3600
+            if time_since_start < 60:
+                time_since_start = str(time_since_start)+' seconds'
+            elif time_since_start < 3600:
+                time_since_start = str(time_since_start//60)+ ':'+str(time_since_start%60)+' minutes'
+            else:
+                time_since_start = str(time_since_start//3600)+ ':'+str((time_since_start%3600)//60)+' hours'
+            print('Time since scraping started - '+time_since_start)
+            print('All time scraping speed - '+('%.0f'%(all_time_scraping_speed))+' movies/hour')
+
+            try:
+                time_since_last_checkpoint = (datetime.now()-time_checkpoint).seconds
+            except:
+                time_since_last_checkpoint = (datetime.now()-scrape_start_time).seconds
+            current_scraping_speed = (25/time_since_last_checkpoint)*3600
+            time_remaining = (time_since_last_checkpoint*((len(titles)-i-j)/25))/(3600)
+            print('Current scraping speed - '+('%.0f'%(current_scraping_speed))+' movies/hour')
+            print('Time remaining as per current speed - '+('%.1f'%(time_remaining))+' hours')
+            print('\n')
+            time_checkpoint = datetime.now()
+        i += 1
+
+    df_movie_synopsys = pd.DataFrame()
     if len(plots) > 0:
-        try:
-            df_movie_synopsys = pd.read_csv(data_folder+'movie_synopsys.csv')
-        except:
-            df_movie_synopsys = pd.DataFrame()
-
         df_movie_synopsys = df_movie_synopsys.append(plots)
-        df_movie_synopsys.to_csv(data_folder+'movie_synopsys.csv', index=False)
-        del df_movie_synopsys
-        plots = []
 
 
 
@@ -149,4 +90,4 @@ def movie_synopsys_scrape(titles):
 
     #################################################################################################################################################################################
 
-    return True
+    return df_movie_synopsys
