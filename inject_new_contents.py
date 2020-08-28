0a2fd1cb9f9169f703743dfabc119960e73df1fc
Bhaskar <bhaskar2choudhary@gmail.com>
2020-08-28 23:11:14 +0530

    added code for committing & pushing to GIT

---
+++ inject_new_contents.py
@@ -0,0 +1,28 @@
+import fileinput
+import re
+import os
+
+
+def update_config(regex_pattern, new_string):
+    filename = 'config.yml'
+    with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+        for line in file:
+            print(re.sub(regex_pattern, new_string, line), end='')
+
+    return True
+
+
+update_config('vCPU:\s\d+$', 'vCPU: 442')
+
+
+def push_to_git(files, commit_message):
+    if type(files) == list:
+        os.system('git add ' + ' '.join(files))
+    elif type(files) == str:
+        os.system('git add ' + files)
+    os.system('git commit -m "' + commit_message + '"')
+    os.system('git push')
+    return True
+
+
+push_to_git(['./config.yml', './inject_new_contents.py'], 'added code for committing & pushing to GIT')
