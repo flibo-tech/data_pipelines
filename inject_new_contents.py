6c8e8ee24577c7cadfac91e81a9892d44f235d92
Bhaskar <bhaskar2choudhary@gmail.com>
2020-09-19 15:46:50 +0530

    added prompt for updating file live_search.csv

--- inject_new_contents.py
+++ inject_new_contents.py
@@ -183,11 +183,11 @@ while go_ahead != 'yes':
     go_ahead = input('\n\x1B[30;41m' + 'Have you moved files full_data.csv & synonyms_similar_contents.csv to ~/ folder on EC2? (yes/no)\x1B[0m\n')
 
 
-go_ahead = input('\n\x1B[30;41m' + 'Have you restarted the servers? (yes/no)\x1B[0m\n')
+go_ahead = input('\n\x1B[30;41m' + 'Have you restarted the servers after updating live_search.csv? (yes/no)\x1B[0m\n')
 while go_ahead != 'yes':
     print('\nConfirm when this script can proceed to next step.')
     time.sleep(1)
-    go_ahead = input('\n\x1B[30;41m' + 'Have you restarted the servers? (yes/no)\x1B[0m\n')
+    go_ahead = input('\n\x1B[30;41m' + 'Have you restarted the servers after updating live_search.csv? (yes/no)\x1B[0m\n')
 
 
 go_ahead = input('\n\x1B[30;41m' + 'Have you removed all the CSVs from /tmp/ folder on EC2? (yes/no)\x1B[0m\n')
