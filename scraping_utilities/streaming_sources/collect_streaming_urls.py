8f19285f486fef75f6188eaf9a389648855ca896
Bhaskar <bhaskar2choudhary@gmail.com>
2020-10-11 18:56:04 +0530

    bug fix

--- scraping_utilities/streaming_sources/collect_streaming_urls.py
+++ scraping_utilities/streaming_sources/collect_streaming_urls.py
@@ -68,7 +68,7 @@ config = yaml.safe_load(open('./../../config.yml'))
 data_folder = config['streaming_sources']
 
 
-def parallelize_dataframe(df, func, n_cores=config['algo']['vCPU']):
+def parallelize_dataframe(df, func, n_cores=16):
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
     df = pd.concat(pool.map(func, df_split))
