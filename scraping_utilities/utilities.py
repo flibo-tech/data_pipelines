5eca8c6477a915eba25d5604b06109576af94597
Bhaskar <bhaskar2choudhary@gmail.com>
2021-02-12 18:23:14 +0530

    bug fix

--- scraping_utilities/utilities.py
+++ scraping_utilities/utilities.py
@@ -725,6 +725,12 @@ def install_requirements_on_remote(public_dns, private_ip, username, key_file):
         interact.send('git checkout develop')
         interact.expect('\(venv_data_collection\)\s+' + default_prompt.replace('~', 'data_pipelines'))
 
+        interact.send('sudo pip-3.6 install -U setuptools')
+        interact.expect('\(venv_data_collection\)\s+' + default_prompt.replace('~', 'data_pipelines'))
+
+        interact.send('sudo pip-3.6 install wheel')
+        interact.expect('\(venv_data_collection\)\s+' + default_prompt.replace('~', 'data_pipelines'))
+
         interact.send('sudo pip-3.6 install -r requirements.txt')
         interact.expect('\(venv_data_collection\)\s+' + default_prompt.replace('~', 'data_pipelines'))
 
